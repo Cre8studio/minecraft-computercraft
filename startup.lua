0166e5482ef643c81e1e3f138f3310a452e7ab70
Duwayne Brown <duwayne@hashtec.co.za>
2021-04-04 15:51:48 +0200

    Update startup.lua
    Search the inventory for a specific item

--- startup.lua
+++ startup.lua
@@ -1,18 +1,65 @@
 print("Welcome to Cre8")
+print("version 0.0.2")
+
+minFuel = 400
 
 local tArgs = {...}
 local doDebug = true
 
+inventory = {}
+
+local fuel = {
+  {name="minecraft:coal", value=80}
+}
+
 function moveForward()
-  if doDebug do
+  if doDebug then
     print("Going forward")
   end
   turtle.dig()
   turtle.forward()
 end
 
-if #tArgs > 0 do 
+function checkFuel()
+  local fuel = turtle.getFuelLevel()
+  local maxFuel = turtle.checkFuelLimit()
+  
+  if fuel <= minFuel then
+    -- run through the inventory to find coal
+    -- use fuel until below fuel limit
+  end
+end
+
+function searchInventory(name)
+  local returnValue = nil
+  
+  for i=0, i<16, 1 do
+    turtle.select(i)
+    inventory[i] = turtle.getItemDetail()
+    if inventory[i].name == name then
+      returnValue = i
+    endls
+  end
+  
+  if doDebug then
+    for i=0, #inventory do
+      print(inventory.name[i] .. ": " .. inventory[i].count)
+    end
+  end
+  
+  return returnValue
+end
+
+function refuel(slot=1)
+  turtle.select(slot)
+  turtle.refuel(1)
+end
+
+if #tArgs > 0 then
   for i=1, tArgs[1], 1 do
     moveForward()
   end
 end
+
+inv = searchInventory("minecraft:coal")
+print(inv)
