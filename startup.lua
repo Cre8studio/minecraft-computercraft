e61f84c403aad12f4dcee0276123d03acf137dd0
Duwayne Brown <duwayne@hashtec.co.za>
2021-04-04 15:10:02 +0200

    Update startup.lua
    Added basic function

--- startup.lua
+++ startup.lua
@@ -1,1 +1,18 @@
-print("Hello, World!")
+print("Welcome to Cre8")
+
+local tArgs = {...}
+local doDebug = true
+
+function moveForward()
+  if doDebug do
+    print("Going forward")
+  end
+  turtle.dig()
+  turtle.forward()
+end
+
+if #tArgs > 0 do 
+  for i=1, tArgs[1], 1 do
+    moveForward()
+  end
+end
